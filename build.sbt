2712576bbc37f533997292c9ba22447e79f5f9e2
anabranch <wac.chambers@gmail.com>
2015-09-11 18:29:19 -0700

    spark1.5, improved loading

--- build.sbt
+++ build.sbt
@@ -4,11 +4,11 @@ version := "0.1"
 
 scalaVersion := "2.11.6"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.1"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.0"
 
-libraryDependencies += "org.apache.spark" %% "spark-graphx" % "1.4.1"
+libraryDependencies += "org.apache.spark" %% "spark-graphx" % "1.5.0"
 
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.4.1"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.5.0"
 
 libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.4.0"
 
--- src/main/scala/part2.scala
+++ src/main/scala/part2.scala
@@ -31,33 +31,60 @@ object FlightDelays extends Serializable {
     val s = s"lag$colName$nRows"
     val lagWindow = Window
       .partitionBy("CallSign")
-      .orderBy(data.col("YearMonthDay"), data.col("Time"))
+      .orderBy(data.col("Date"))
 
     lag(data.col(colName), nRows).over(lagWindow).alias(s)
   }
 
   def generateSum(data: DataFrame, colName: String, nRows:Int) = {
     val s = s"sum$colName$nRows"
-    val sumWindow1 = Window
+    val sumWindow = Window
       .partitionBy("CallSign")
-      .orderBy("YearMonthDay", "Time")
+      .orderBy(data.col("Date"))
       .rowsBetween(-1 * nRows,0)
 
-      sum(data.col(colName)).over(sumWindow1).alias(s)
+    sum(data.col(colName)).over(sumWindow).alias(s)
+  }
+
+  def generateAvg(data: DataFrame, colName: String, nRows:Int) = {
+    val s = s"sum$colName$nRows"
+    val sumWindow = Window
+      .partitionBy("CallSign")
+      .orderBy(data.col("Date"))
+      .rowsBetween(-1 * nRows,0)
+
+    avg(data.col(colName)).over(sumWindow).alias(s)
   }
 
   def loadWeatherData(sqlContext: org.apache.spark.sql.SQLContext) = {
 
-    // load up the data!
+    val makeDate = udf( (ymd: String, time:String, tz:String) => {
+      val y = ymd.slice(0,4)
+      val m = ymd.slice(4,6)
+      val d = ymd.slice(6,8)
+      val h = time.slice(0,2)
+      val mm = time.slice(2,4)
+      val hrs = tz.split("\\.")(0).toFloat
+      val mins = ("0." + tz.split("\\.")(1)).toFloat * 60
+      s"$y-$m-$d" + "T" + s"$h:$mm" + "+" + hrs + ":" + mins
+    })
+
     var rawWeatherData = sqlContext.read.format("com.databricks.spark.csv")
       .option("header","true")
       .load("s3n://b-datasets/weather_data/*hourly.txt")
-      .repartition(36)
+
+    var airportMeta = sqlContext.read.format("com.databricks.spark.csv").load("s3n://b-datasets/airports.csv")
+
+    Array("ID","NAME","CITY","COUNTRY","FAA","ICAO","LAT","LON","ALT","TZ","DST","TZ2").zipWithIndex.foreach { x =>
+      val (name, cur) = x
+      airportMeta = airportMeta.withColumnRenamed(s"C$cur", name)
+    }
+
+    rawWeatherData = cleanColumnNames(rawWeatherData, sqlContext)
+      .withColumnRenamed("WbanNumber", "ID")
+    
     val newWeatherTypes = Map(("WindSpeedkt", "Float"), ("DryBulbTemp", "Float"),
       ("DewPointTemp", "Float"), ("WetBulbTemp", "Float"),("PrecipTotal", "Float"))
-
-    rawWeatherData = cleanColumnNames(rawWeatherData,sqlContext)
-      .withColumnRenamed("WbanNumber", "ID")
 
     rawWeatherData = convertColumns(rawWeatherData, newWeatherTypes)
 
@@ -71,9 +98,11 @@ object FlightDelays extends Serializable {
       .withColumnRenamed("Call Sign", "CallSign")
     // we are assuming that the station names/locations haven't changed since we're grabbing distinct
 
-    rawWeatherData
+    rawWeatherData = rawWeatherData
       .join(rawStationList,
         rawWeatherData.col("ID") === rawStationList.col("ID1"), "outer")
+
+    rawWeatherData
       .drop("ID")
       .drop("ID1")
       .drop("StationType")
@@ -89,23 +118,39 @@ object FlightDelays extends Serializable {
       .drop("PressureTendency")
       .drop("SeaLevelPressure")
       .drop("RecordType")
-      .repartition(36)
+      .repartition(24)
   }
 
   def loadFlightData(sqlContext: org.apache.spark.sql.SQLContext) = {
 
-    val rawFlightData = sqlContext
+    val makeDate = udf( (y: String, m:String, d:String, time:String, tz:String) => {
+      val h = time.slice(0,2)
+      val mm = time.slice(2,4)
+      val hrs = tz.split("\\.")(0).toFloat
+      val mins = ("0." + tz.split("\\.")(1)).toFloat * 60
+      s"$y-$m-$d" + "T" + s"$h:$mm" + "+" + hrs + ":" + mins
+    })
+
+    var rawFlightData = sqlContext
       .read.format("com.databricks.spark.csv")
       .option("header","true")
       .load("s3n://b-datasets/flight_data/*")
 
-    val newFlightTypes = Map(
-      ("Year", "int"), ("Month", "int"), ("DayofMonth","int"),
-      ("DayOfWeek","int"), ("WeatherDelay", "int")
-    )
+    val newFlightTypes = Map(("WeatherDelay", "int"))
+
+    var airportMeta = sqlContext.read.format("com.databricks.spark.csv").load("s3n://b-datasets/airports.csv")
+
+    Array("ID","NAME","CITY","COUNTRY","FAA","ICAO","LAT","LON","ALT","TZ","DST","TZ2").zipWithIndex.foreach { x =>
+      val (name, cur) = x
+      airportMeta = airportMeta.withColumnRenamed(s"C$cur", name)
+    }
 
     // just remove all these extra columns to keep things neat
-    convertColumns(rawFlightData, newFlightTypes)
+    rawFlightData = convertColumns(rawFlightData, newFlightTypes)
+      .join(airportMeta.select($"FAA",$"TZ"),
+        airportMeta.col("FAA") === rawFlightData.col("Origin"), "outer")
+
+    rawFlightData
       .drop("LateAircraftDelay")
       .drop("SecurityDelay")
       .drop("NASDelay")
@@ -126,32 +171,37 @@ object FlightDelays extends Serializable {
       .drop("ArrDelay")
       .drop("DepDelay")
       .drop("Distance")
-      .repartition(36)
+      .drop("DayOfWeek")
+      .repartition(24)
   }
 
-  def main(args: Array[String]) = {
-    val conf = new SparkConf().setAppName("FlightDelayPredictor")
-    val sc = new SparkContext(conf)
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+  // def main(args: Array[String]) = {
+  //   val conf = new SparkConf().setAppName("FlightDelayPredictor")
+  //   val sc = new SparkContext(conf)
+  //   val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-  var weatherData = loadWeatherData(sqlContext)
-  var flightData = loadFlightData(sqlContext)
-  weatherData.write.parquet("weatherData.parquet")
-  flightData.write.parquet("flightData.parquet")
-  weatherData = sqlContext.read.parquet("weatherData.parquet").repartition(24)
-  flightData = sqlContext.read.parquet("flightData.parquet").repartition(24)
-  weatherData.cache()
-  flightData.cache()
-  weatherData.registerTempTable("rawWeather")
-  flightData.registerTempTable("rawFlight")
+    var weatherData = loadWeatherData(sqlContext)
+    var flightData = loadFlightData(sqlContext)
+    weatherData.write.parquet("weatherData.parquet")
+    flightData.write.parquet("flightData.parquet")
+    var weatherData = sqlContext.read.parquet("weatherData.parquet").repartition(24)
+    var flightData = sqlContext.read.parquet("flightData.parquet").repartition(24)
+    weatherData.cache()
+    flightData.cache()
+    weatherData.registerTempTable("rawWeather")
+    flightData.registerTempTable("rawFlight")
+
 
 
 
     weatherData.select(
       $"CallSign",
-      $"YearMonthDay",
-      $"Time",
+      $"Date",
       $"WindSpeedkt",
+      $"PrecipTotal",
+      $"DryBulbTemp",
+      $"DewPointTemp",
+      $"WetBulbTemp",
       generateLag(weatherData, "WindSpeedkt", 1),
       generateSum(weatherData, "WindSpeedkt", 1),
       generateLag(weatherData, "WindSpeedkt", 3),
@@ -165,19 +215,47 @@ object FlightDelays extends Serializable {
       generateLag(weatherData, "WindSpeedkt", 36),
       generateSum(weatherData, "WindSpeedkt", 36),
       generateLag(weatherData, "WindSpeedkt", 72),
-      generateSum(weatherData, "WindSpeedkt", 72)
-  ).show
+      generateSum(weatherData, "WindSpeedkt", 72),
+      generateLag(weatherData, "PrecipTotal", 1),
+      generateSum(weatherData, "PrecipTotal", 1),
+      generateLag(weatherData, "PrecipTotal", 3),
+      generateSum(weatherData, "PrecipTotal", 3),
+      generateLag(weatherData, "PrecipTotal", 6),
+      generateSum(weatherData, "PrecipTotal", 6),
+      generateLag(weatherData, "PrecipTotal", 12),
+      generateSum(weatherData, "PrecipTotal", 12),
+      generateLag(weatherData, "PrecipTotal", 24),
+      generateSum(weatherData, "PrecipTotal", 24),
+      generateLag(weatherData, "PrecipTotal", 36),
+      generateSum(weatherData, "PrecipTotal", 36),
+      generateLag(weatherData, "PrecipTotal", 72),
+      generateSum(weatherData, "PrecipTotal", 72),
+      generateAvg(weatherData, "DryBulbTemp", 1),
+      generateAvg(weatherData, "DryBulbTemp", 3),
+      generateAvg(weatherData, "DryBulbTemp", 6),
+      generateAvg(weatherData, "DryBulbTemp", 12),
+      generateAvg(weatherData, "DryBulbTemp", 24),
+      generateAvg(weatherData, "DryBulbTemp", 36),
+      generateAvg(weatherData, "DryBulbTemp", 72),
+      generateAvg(weatherData, "DewPointTemp", 1),
+      generateAvg(weatherData, "DewPointTemp", 3),
+      generateAvg(weatherData, "DewPointTemp", 6),
+      generateAvg(weatherData, "DewPointTemp", 12),
+      generateAvg(weatherData, "DewPointTemp", 24),
+      generateAvg(weatherData, "DewPointTemp", 36),
+      generateAvg(weatherData, "DewPointTemp", 72),
+      generateAvg(weatherData, "WetBulbTemp", 1),
+      generateAvg(weatherData, "WetBulbTemp", 3),
+      generateAvg(weatherData, "WetBulbTemp", 6),
+      generateAvg(weatherData, "WetBulbTemp", 12),
+      generateAvg(weatherData, "WetBulbTemp", 24),
+      generateAvg(weatherData, "WetBulbTemp", 36),
+      generateAvg(weatherData, "WetBulbTemp", 72)
+    ).show
 
 
 
+    // need to convert to LabeledPoint in MLLib
 
-  sqlContext.sql("""SELECT CallSign, PrecipTotal, sum(PrecipTotal) over w from rawWeather;
-WINDOW w as (PARTITION BY (YearMonthDay, Time) ORDER BY (YearMonthDay, Time) ROWS BETWEEN 3 PRECEDING AND CURRENT ROW)""")
-
-
-  // need to convert to LabeledPoint in MLLib
-  // we might need a hashingTF to convert the origins/destinations or something like that. Not exactly sure how to handle it.
-  // this is going to be a like a one hot encoder or something
-
-}
+  }
 }
